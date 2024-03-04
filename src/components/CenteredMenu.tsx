28c658d5088bf11666c98e826cb4fb855c32faad
Rémi Weng <ixartz@users.noreply.github.com>
2024-03-04 23:06:53 +0100

    feat: add github and twitter icon in the right menu

--- src/components/CenteredMenu.tsx
+++ src/components/CenteredMenu.tsx
@@ -3,20 +3,13 @@ import Link from 'next/link';
 const CenteredMenu = (props: {
   logo: React.ReactNode;
   children: React.ReactNode;
-  rightMenu: React.ReactNode;
 }) => (
   <div className="flex items-center justify-between">
     <Link href="/">{props.logo}</Link>
 
-    <nav>
-      <ul className="flex flex-row items-center gap-x-6 text-lg font-medium [&_li:hover]:opacity-100 [&_li]:opacity-60">
-        {props.children}
-      </ul>
-    </nav>
-
     <div>
-      <ul className="flex flex-row items-center gap-x-6 text-lg font-medium [&_li:not(:last-child):hover]:opacity-100 [&_li:not(:last-child)]:opacity-60">
-        {props.rightMenu}
+      <ul className="flex flex-row gap-x-4 text-muted-foreground [&_svg:hover]:text-primary [&_svg:hover]:opacity-100 [&_svg]:size-5 [&_svg]:fill-current [&_svg]:opacity-80">
+        {props.children}
       </ul>
     </div>
   </div>
